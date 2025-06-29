1f218373685cba28e3b773b49c0d737a362e1623
Pushkar singhal <pusinghal010@gmail.com>
2025-06-29 14:18:57 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        TicTacToe tictactoe = new TicTacToe();
+        // created a object for TicTacToe class in App class
+    }
+}
