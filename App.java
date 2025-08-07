4f64299d0dbbe0a8b2cadd61f5ba8a503e5f0f75
Pushkar singhal <pusinghal010@gmail.com>
2025-08-07 21:34:54 +0530

    Delete App.java

--- App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        TicTacToe tictactoe = new TicTacToe();
-        // created a object for TicTacToe class in App class
-    }
-}
